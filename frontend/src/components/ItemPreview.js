7be32ebb260a0f7944f0013506ea2e2f745d5307
Kasim Shaikh <kasimalishaikh786786@gmail.com>
2022-08-12 18:50:59 +0530

    fix img

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={ placeholder}
+        src={item.image || placeholder}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
