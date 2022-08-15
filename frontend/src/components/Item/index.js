7d96c5d68b37f071b1da6b0de0eed7a3683d9424
Kasim Shaikh <kasimalishaikh786786@gmail.com>
2022-08-15 16:35:29 +0530

    revert

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -9,7 +9,6 @@ import {
   ITEM_PAGE_UNLOADED,
 } from "../../constants/actionTypes";
 
-
 const mapStateToProps = (state) => ({
   ...state.item,
   currentUser: state.common.currentUser,
@@ -51,7 +50,7 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image || "./placeholder.png"}
+                src={this.props.item.image}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
